e9ea1a1fe31281e308c1c0907b7d123a7e1c3a85
xiaoyu0722 <xiaoyu.liu@sap.com>
2026-04-30 11:37:49 +0800

    fix: 🐛 typo issue

--- Sources/FioriSwiftUICore/_FioriStyles/AttachmentGroupStyle.fiori.swift
+++ Sources/FioriSwiftUICore/_FioriStyles/AttachmentGroupStyle.fiori.swift
@@ -14,7 +14,7 @@ public struct AttachmentGroupBaseStyle: AttachmentGroupStyle {
             configuration.title
                 .id("Attachment:Title:\(configuration.title)")
                 .accessibilityIdentifier("Attachment:Title:\(configuration.title)")
-                .accessibilityHint(configuration.controlState == .readOnly ? "ready only".localizedFioriString() : "actions available".localizedFioriString())
+                .accessibilityHint(configuration.controlState == .readOnly ? "read only".localizedFioriString() : "actions available".localizedFioriString())
                 .padding(.bottom, AttachmentConstants.extraTitleBottomPadding)
             
             LazyVGrid(columns: Array(repeating: GridItem(.adaptive(minimum: AttachmentConstants.thumbnailWidth), alignment: .topLeading), count: 1), spacing: AttachmentConstants.cellVerticalSpacing) {
